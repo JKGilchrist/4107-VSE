f1b5b2407a2a98c8dca9470b8ec8970d419947fd
JKGilchrist <51573693+JKGilchrist@users.noreply.github.com>
2020-02-11 16:57:59 -0500

    Modules 2-4 complete

--- dictionary_builder.py
+++ dictionary_builder.py
@@ -3,14 +3,17 @@
 import pandas as pd
 import string_formatting
 import sys
-
-
+import pickle
 
 def open_file():
-    df = pd.read_csv("corpus.csv", sep = "|")
+    df = pd.read_csv("save_files/corpus.csv", sep = "|")
 
     return df
 
+def save(set1, name):
+    pickle.dump(set1, open("save_files/{name}.obj".format(name = name), "wb"  ) )
+    
+    
 if __name__ == "__main__": 
     df = open_file()
 
@@ -22,17 +25,18 @@ if __name__ == "__main__":
     for title in titles:
         tokens = string_formatting.get_formatted_tokens(title)
         title_dic.update(tokens)
-    title_dic.remove('')
-    print(title_dic)
-    #TODO later
-    if False:
-        descriptions = []
-        for x in df["description"]:
-            descriptions.append(x)
     
-    
-        
+    save(title_dic, "title_dic")
 
+    descriptions = []
+    for x in df["description"]:
+        descriptions.append(x)
 
+    description_dic = set()
+    for description in descriptions:
+        tokens = string_formatting.get_formatted_tokens(description)
+        description_dic.update(tokens)
+
+    save(description_dic, "description_dic")
 
 
