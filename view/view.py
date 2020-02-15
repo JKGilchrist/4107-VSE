fe75d07fe92dc16c3cfc529788e554396c03f2e9
Beatrice084 <bjohn084@uottawa.ca>
2020-02-15 14:46:44 -0500

    update view

--- view/view.py
+++ view/view.py
@@ -1,5 +1,53 @@
 import tkinter as tk
 
+
+class ListItem(tk.Frame):
+    def __init__(self, master, id, title, description, x, y):
+        tk.Frame.__init__(self, master, bg='white', relief='ridge', bd=2)
+
+        self.item(id, title, description, x, y)
+
+    def item(self, id, title, description, x, y):
+        tk.Button(self.master,
+                 text=str(id) + ": " + title,
+                 justify=tk.LEFT,
+                 padx=20,
+                 command= lambda: self.Create_Toplevel(id, title, description))\
+            .place(x=x, y=y)
+        tk.Label(self.master,
+                 text=description[0:200] + " ...",
+                 wraplength = 500,
+                 justify=tk.LEFT)\
+            .place(x=x, y=y+30)
+
+    # modified from https://stackoverflow.com/questions/16803686/how-to-create-a-modal-dialog-in-tkinter
+    def Create_Toplevel(self, id, title, description):
+
+        # THE CLUE
+        self.master.wm_attributes("-disabled", True)
+
+        # Creating the toplevel dialog
+        self.toplevel_dialog = tk.Toplevel(self)
+        self.toplevel_dialog.minsize(300, 100)
+        self.toplevel_dialog.transient(self)
+        self.toplevel_dialog.protocol("WM_DELETE_WINDOW", self.Close_Toplevel)
+        self.toplevel_dialog_label = tk.Label(self.toplevel_dialog, text=str(id) + ": " + title)
+        self.toplevel_dialog_label.pack(side='top')
+        self.toplevel_dialog_label = tk.Label(self.toplevel_dialog, text=description, wraplength = 300)
+        self.toplevel_dialog_label.pack(side='top')
+        self.toplevel_dialog_yes_button = tk.Button(self.toplevel_dialog, text='Close', command=self.Close_Toplevel)
+        self.toplevel_dialog_yes_button.pack(side='left', fill='x', expand=True)
+
+    def Close_Toplevel(self):
+
+        # IMPORTANT!
+        self.master.wm_attributes("-disabled", False) # IMPORTANT!
+
+        self.toplevel_dialog.destroy()
+
+        # Possibly not needed, used to focus parent window again
+        self.master.deiconify()
+
 class GUI(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
@@ -60,6 +108,13 @@ class GUI(tk.Frame):
                        padx=20,
                        variable=self.w,
                        value=2).place(x=480, y=100)
+
+        ListItem(root,
+                 1,
+                 "ADM 1100 Introduction to Business Management (3 units)",
+                 "This course provides the student with the basic knowledge necessary to effectively manage an organization. The student will learn what constitutes the manager's role and how the managerial functions of planning, organizing, leading, controlling, and communication are used to oversee the organization's human, financial, physical, material, and commercial resources. In particular, through the case method approach, students will be introduced to the art of logical problem solving, while addressing such issues as corporate social responsibility and managerial ethics.",
+                 280,
+                 140)
 
 
 if __name__ == '__main__':
